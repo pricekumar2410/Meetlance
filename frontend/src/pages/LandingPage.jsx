eeb03bb6a97aac6498de8cea4a305727c991c401
princekumar249 <princekumar746586@gmail.com>
2026-01-17 09:28:04 +0530

    feat: setup react frontend with vite

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../App.css"
+
+export default function LandingPage() {
+  return (
+    <div className='LandingPageContainer'>
+      <h1 style={{ color: "white" }}>Welcome back to Meet<span style={{ color: "red" }}>lance</span></h1>
+    </div>
+  )
+}
