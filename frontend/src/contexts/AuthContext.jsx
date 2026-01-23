0e51e0b96d6d463a0ad3b2d197333f26d527e589
princekumar249 <princekumar746586@gmail.com>
2026-01-24 00:31:24 +0530

    Updated URL

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -2,13 +2,13 @@ import axios from "axios";
 import httpStatus from "http-status";
 import { useNavigate } from "react-router-dom";
 import { createContext, useContext, useState } from "react";
-
+import server from "../environment";
 
 
 export const AuthContext = createContext({});
 
 const client = axios.create({
-    baseURL: "http://localhost:3000/api/v1/users"
+    baseURL: `${server}/api/v1/users`
 })
 
 export const AuthProvider = ({ children }) => {
---
+++ frontend/src/environment.js
@@ -0,0 +1,9 @@
+let IS_PROD = true;
+
+const server = IS_PROD ?
+    "https://meetlance-backend.onrender.com" :
+    "http://localhost:3000"
+
+
+
+export default server;
